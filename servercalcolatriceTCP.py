e98b649c93c08df85d0e336613fc7a9078ef2569
GostivariMattia <mattia.gostivari@alessandrinimainardi.edu.it>
2024-01-24 13:41:53 +0100

    aggiustamento

--- servercalcolatriceTCP.py
+++ servercalcolatriceTCP.py
@@ -1,7 +1,7 @@
 import socket,json
 
 SERVER_IP="127.0.0.1"
-SERVER_PORT=5005
+SERVER_PORT=20009
 BUFFER_SIZE=1024
 
 #creazione della socket
@@ -9,12 +9,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_server:
     sock_server.bind((SERVER_IP,SERVER_PORT))
     sock_server.listen()
     print(f"server in ascolto su {SERVER_IP}:{SERVER_PORT}")
+
     while True:
         sock_service, address_client = sock_server.accept()
+        print(f"connessione ricevuta da {address_client}")
+
         with sock_service as sock_client:
             while True:
                 dati = sock_client.recv(BUFFER_SIZE).decode()
+                if not dati:
+                    break
                 dati=json.loads(dati)
+
                 primoNumero=dati['primoNumero']
                 operazione=dati['operazione']
                 secondoNumero=dati['secondoNumero']
