d82f3737add9d3160287f828adf0f486a9d77824
GostivariMattia <mattia.gostivari@alessandrinimainardi.edu.it>
2024-01-26 09:07:32 +0100

    calcolatrice 3.0

---
+++ calcolatrice3.0/clientcalcolatrice3.0.py
@@ -0,0 +1,24 @@
+import socket,json
+
+SERVER_IP="127.0.0.1"
+SERVER_PORT=5005
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_service:
+    sock_service.connect((SERVER_IP, SERVER_PORT))
+    while True:
+        primoNumero=float(input("inserisci il primo numero"))
+        operazione=input("inserisci l'operazione (+,-,*,/,%)")
+        secondoNumero=float(input("inserisci il secondo numero"))
+        messaggio={'primoNumero': primoNumero,
+                'operazione':operazione,
+                'secondoNumero':secondoNumero}
+        messaggio=json.dumps(messaggio)
+        sock_service.sendall(messaggio.encode("UTF-8"))
+        data=sock_service.recv(1024)
+        print('Received', data.decode())
+        risp=input("fare un altra operazione? s=si n=no")
+        if (risp=='n'):
+            break
+
+print("socket chiusa")
---
+++ calcolatrice3.0/servercalcolatrice3.0.py
@@ -0,0 +1,65 @@
+import socket,json
+from threading import Thread
+
+SERVER_IP="127.0.0.1"
+SERVER_PORT=5005
+BUFFER_SIZE=1024
+
+
+def ricevi_comandi(sock_service,addr_client):
+    with sock_service as sock_client:
+        while True:
+            dati = sock_client.recv(BUFFER_SIZE).decode()
+            if not dati:
+                print("termine elaborazioni")
+                break
+            dati=json.loads(dati)
+            primoNumero=dati['primoNumero']
+            operazione=dati['operazione']
+            secondoNumero=dati['secondoNumero']
+            if operazione=='+':
+                risultato=primoNumero+secondoNumero
+            elif operazione=='-':
+                risultato=primoNumero-secondoNumero
+            elif operazione=='*':
+                risultato=primoNumero*secondoNumero
+            elif operazione=='/':
+                if secondoNumero!=0:
+                    risultato=primoNumero/secondoNumero
+                else:
+                    risultato="impossibile"
+            elif operazione=='%':
+                risultato=primoNumero%secondoNumero
+
+            sock_client.sendall(str(risultato).encode())
+
+
+def ricevi_connessioni(sock_listen):
+    while True:
+        sock_service,addr_client=sock_listen.accept()
+        print("\nConnessione ricevuta da %s" % str(addr_client))
+        print("creo un thread per servire le richieste")
+        try:
+            Thread(target=ricevi_comandi, args=(sock_service,addr_client)).start()
+        except:
+            print("il thread non si avvia")
+            sock_listen.close()
+
+
+def avvia_server(indirizzo,porta):
+    try:
+        sock_listen=socket.socket()
+        sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+        sock_listen.bind((indirizzo,porta))
+        sock_listen.listen(5)
+        print("server in attesa di connessioni")
+        ricevi_connessioni(sock_listen)
+    except socket.error as errore:
+        print(f"errore: {errore}")
+
+    ricevi_connessioni(sock_listen)
+
+
+if __name__=='__main__':
+    avvia_server(SERVER_IP,SERVER_PORT)
+
